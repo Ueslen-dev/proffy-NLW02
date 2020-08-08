9bb792bb60e0986075c790f847d95cafcad88073
ueslen santos <ueslen@zygon.digital>
2020-08-08 19:57:33 -0300

    aplication is done

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,34 +2,55 @@ import React from 'react';
 import TeacherForm from '../../pages/TeacherForm';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
+
 import './styles.css';
 
-const TeacherItem = () => {
+
+export interface Teacher{
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    user_id: number;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection(){
+        api.post('/connections',{
+            user_id: teacher.id
+        })
+    }
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4" alt="Diego Fernandes"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Diego Fernandes</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, ab.
-                <br/><br/>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis dolores earum, iste tenetur pariatur excepturi cumque! Eligendi assumenda magnam id?
+                {teacher.bio}
             </p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp icon"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
